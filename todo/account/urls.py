a16ae816b09e0a4647711e8c0e8799fe7d3ab8be
AbdunazarovHikmatullo <abdunazarovhikmatullo2@gmail.com>
2025-12-25 00:15:13 +0500

    aside lists added and added list header

--- todo/account/urls.py
+++ todo/account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import register, login_view
-
+from django.contrib.auth import logout
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_view, name='login'),
+    path('logout/', logout, name='logout')
 ]
