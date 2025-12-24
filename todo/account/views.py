daad03fa4e7452b6dd4a1b6cd7e0f73cc7193f87
AbdunazarovHikmatullo <abdunazarovhikmatullo2@gmail.com>
2025-12-24 17:27:02 +0500

    first commit

---
+++ todo/account/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+
+def register(request):
+    return render(request, 'account/register.html')
+
+def login(request):
+    return render(request, 'account/login.html')
